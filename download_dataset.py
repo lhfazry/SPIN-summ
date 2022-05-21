ed810ab0da29529db496889d7315cb965c88d40e
Fazry <lhuqita.fazry@gmail.com>
2022-05-21 13:06:59 +0700

    fixing bugs

--- download_dataset.py
+++ download_dataset.py
@@ -1,3 +1,7 @@
 import gdown
 
-gdown.download('https://drive.google.com/u/0/uc?id=1b3rmCSIoh6VhD4HKWjI4HOW-cSwcwbeC&export=download')
+#arxiv
+#gdown.download('https://drive.google.com/u/0/uc?id=1b3rmCSIoh6VhD4HKWjI4HOW-cSwcwbeC&export=download')
+
+#pubmed
+gdown.download('https://drive.google.com/u/0/uc?id=1lvsqvsFi3W-pE1SqNZI0s8NR9rC1tsja&export=download&confirm=t')
