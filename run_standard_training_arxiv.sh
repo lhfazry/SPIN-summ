24dd0e62eaa2bade8077dbabbbab8ebd4fe9e041
Fazry <lhuqita.fazry@gmail.com>
2022-05-23 20:38:18 +0700

    fixing bugs

--- run_standard_training_arxiv.sh
+++ run_standard_training_arxiv.sh
@@ -3,10 +3,11 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --model_name_or_path google/bigbird-pegasus-large-bigpatent --tokenizer_name google/bigbird-pegasus-large-bigpatent \
     --do_train \
     --do_eval \
-    --train_file dataset/arxiv/train.json \
-    --validation_file dataset/arxiv/val.json \
     --task summarization \
-    --max_train_samples 1000 \
+    --train_file dataset/arxiv/standard/train.json \
+    --validation_file dataset/arxiv/standard/val.json \
+    --text_column document \
+    --summary_column summary \
     --output_dir standard_arxiv_pre/models --logging_dir standard_arxiv_pre/models/logs \
     --seed 100 \
     --per_device_train_batch_size=8 \
@@ -18,6 +19,6 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --adafactor \
     --max_source_length 512 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length \
     --num_beams 3 \
-    --num_train_epochs 3 --save_step 100 --save_total_limit 1 \
+    --num_train_epochs 2 --save_strategy epoch --save_total_limit 1 \
     --load_best_model_at_end \
     --evaluation_strategy epoch --metric_for_best_model rouge2 --greater_is_better true
