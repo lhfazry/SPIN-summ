12cf5f210c0a226e1e1385dd012137aecd01d25c
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 04:30:43 +0700

    fixing bugs

--- run_standard_training_pubmed.sh
+++ run_standard_training_pubmed.sh
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --per_device_eval_batch_size=8 \
     --overwrite_output_dir \
     --predict_with_generate \
-    --max_val_samples 35 \
+    --max_val_samples 17 \
     --learning_rate 1e-4 \
     --adafactor \
     --max_source_length 512 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length \
