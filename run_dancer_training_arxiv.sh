899b88e02646f15380b2767183f521bec9d06937
Fazry <lhuqita.fazry@gmail.com>
2022-05-23 08:57:11 +0700

    fixing epochs

--- run_dancer_training_arxiv.sh
+++ run_dancer_training_arxiv.sh
@@ -10,8 +10,8 @@ python src/run_summarization.py \
     --summary_column summary \
     --output_dir dancer_arxiv_pre/models --logging_dir dancer_arxiv_pre/models/logs \
     --seed 100 \
-    --per_device_train_batch_size=4 \
-    --per_device_eval_batch_size=4 \
+    --per_device_train_batch_size=16 \
+    --per_device_eval_batch_size=16 \
     --overwrite_output_dir \
     --predict_with_generate \
     --max_val_samples 251 \
@@ -19,6 +19,6 @@ python src/run_summarization.py \
     --adafactor \
     --max_source_length 512 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length \
     --num_beams 3 \
-    --num_train_epochs 2 --save_step 100 --save_total_limit 1 \
+    --num_train_epochs 2 --save_step 4436 --save_total_limit 1 \
     --load_best_model_at_end \
-    --evaluation_strategy steps --eval_steps 100 --metric_for_best_model rouge2 --greater_is_better true
+    --evaluation_strategy steps --eval_steps 4436 --metric_for_best_model rouge2 --greater_is_better true
