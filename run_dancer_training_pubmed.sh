a15043f01ada859a9bb4bae38d73b96487b065f5
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 14:47:32 +0700

    fixing bugs

--- run_dancer_training_pubmed.sh
+++ run_dancer_training_pubmed.sh
@@ -12,8 +12,9 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --output_dir dancer_pubmed_pre/models \
     --logging_dir dancer_pubmed_pre/models/logs \
     --seed 100 \
-    --per_device_train_batch_size=8 \
-    --per_device_eval_batch_size=8 \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
+    --overwrite_output_dir \
     --predict_with_generate \
     --max_val_samples 35 \
     --learning_rate 1e-4 \
