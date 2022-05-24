dec5f92ace41e0d12a316f69491346803ab5d7dd
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 10:18:13 +0700

    fixing bugs

--- run_dancer_training_pubmed.sh
+++ run_dancer_training_pubmed.sh
@@ -12,13 +12,12 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --seed 100 \
     --per_device_train_batch_size=8 \
     --per_device_eval_batch_size=8 \
-    --overwrite_output_dir \
     --predict_with_generate \
     --max_val_samples 35 \
     --learning_rate 1e-4 \
     --adafactor \
     --max_source_length 512 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length \
     --num_beams 3 \
-    --num_train_epochs 2 --save_strategy epoch --save_total_limit 1 \
+    --num_train_epochs 50 --save_strategy epoch --save_total_limit 1 \
     --load_best_model_at_end \
     --evaluation_strategy epoch --metric_for_best_model rouge2 --greater_is_better true
