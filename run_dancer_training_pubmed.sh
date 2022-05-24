3b0e43ce46feb8c8cbffba605757fa864f4d62aa
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 20:34:33 +0700

    fixing bugs

--- run_dancer_training_pubmed.sh
+++ run_dancer_training_pubmed.sh
@@ -14,15 +14,15 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --output_dir dancer_pubmed_pre/models \
     --logging_dir dancer_pubmed_pre/models/logs \
     --seed 100 \
-    --per_device_train_batch_size=8 \
-    --per_device_eval_batch_size=8 \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
     --gradient_accumulation_steps=16 \
     --gradient_checkpointing true \
     --fp16 true \
     --overwrite_output_dir \
     --predict_with_generate \
     --learning_rate 1e-4 \
-    --adafactor \
+    --optim adafactor \
     --max_source_length 4096 \
     --max_target_length 256 \
     --val_max_target_length 256  \
