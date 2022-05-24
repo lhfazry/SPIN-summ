fbd83992b3f97f300a44249585787cb0d7235484
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 14:43:30 +0700

    fixing bugs

--- run_dancer_training_arxiv.sh
+++ run_dancer_training_arxiv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
-    --model_name_or_path google/bigbird-pegasus-large-bigpatent --tokenizer_name google/bigbird-pegasus-large-bigpatent \
+    --model_name_or_path google/bigbird-pegasus-large-bigpatent \
+    --tokenizer_name google/bigbird-pegasus-large-bigpatent \
     --do_train \
     --do_eval \
     --task summarization \
@@ -8,7 +9,7 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --validation_file dataset/arxiv/processed/arxiv/val.json \
     --text_column document \
     --summary_column summary \
-    --output_dir dancer_arxiv_pre/models 
+    --output_dir dancer_arxiv_pre/models \
     --logging_dir dancer_arxiv_pre/models/logs \
     --seed 100 \
     --per_device_train_batch_size=8 \
@@ -18,13 +19,13 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --max_val_samples 251 \
     --learning_rate 1e-4 \
     --adafactor \
-    --max_source_length 4096 
-    --max_target_length 128 
-    --val_max_target_length 128 
+    --max_source_length 4096 \
+    --max_target_length 128 \
+    --val_max_target_length 128 \
     --pad_to_max_length \
     --num_beams 3 \
-    --num_train_epochs 2 
-    --save_strategy epoch  
+    --num_train_epochs 2 \
+    --save_strategy epoch  \
     --save_total_limit 1 \
     --load_best_model_at_end \
     --evaluation_strategy epoch \
--- run_standard_training_pubmed.sh
+++ run_standard_training_pubmed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
-    --model_name_or_path google/bigbird-pegasus-large-bigpatent --tokenizer_name google/bigbird-pegasus-large-bigpatent \
+    --model_name_or_path google/bigbird-pegasus-large-bigpatent \
+    --tokenizer_name google/bigbird-pegasus-large-bigpatent \
     --do_train \
     --do_eval \
     --task summarization \
@@ -8,7 +9,8 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --validation_file dataset/pubmed/standard/pubmed/val.json \
     --text_column document \
     --summary_column summary \
-    --output_dir standard_pubmed_pre/models --logging_dir standard_pubmed_pre/models/logs \
+    --output_dir standard_pubmed_pre/models \
+    --logging_dir standard_pubmed_pre/models/logs \
     --seed 100 \
     --per_device_train_batch_size=8 \
     --per_device_eval_batch_size=8 \
@@ -17,8 +19,15 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --max_val_samples 17 \
     --learning_rate 1e-4 \
     --adafactor \
-    --max_source_length 512 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length \
+    --max_source_length 4096 \
+    --max_target_length 128 \
+    --val_max_target_length 128 \
+    --pad_to_max_length \
     --num_beams 3 \
-    --num_train_epochs 2 --save_strategy epoch --save_total_limit 1 \
+    --num_train_epochs 50 \ 
+    --save_strategy epoch \
+    --save_total_limit 1 \
     --load_best_model_at_end \
-    --evaluation_strategy epoch --metric_for_best_model rouge2 --greater_is_better true
+    --evaluation_strategy epoch \
+    --metric_for_best_model rouge2 \
+    --greater_is_better true
