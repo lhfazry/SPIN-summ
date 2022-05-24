c252099ecc7587338c95e0b4081e550fe34b8a34
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 10:36:01 +0700

    fixing bugs

---
+++ run_dancer_generation_arxiv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python src/dancer_generation.py \
+    --mode dancer \
+    --model_path dancer_arxiv_pre/models \
+    --output_path dancer_arxiv_pre/generation \
+    --data_path dataset/arxiv/processed/arxiv/test.json \
+    --text_column document \
+    --summary_column summary \
+    --write_rouge 1 \
+    --seed 100 \
+    --test_batch_size 8 \
+    --max_source_length 512 --max_summary_length 128 \
+    --num_beams 5
---
+++ run_standard_generation_arxiv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python src/dancer_generation.py \
+    --mode standard \
+    --model_path standard_arxiv_pre/models \
+    --output_path standard_arxiv_pre/generation \
+    --data_path dataset/arxiv/standard/arxiv/test.json \
+    --text_column document \
+    --summary_column summary \
+    --write_rouge 1 \
+    --seed 100 \
+    --test_batch_size 8 \
+    --max_source_length 1024 --max_summary_length 256 \
+    --num_beams 5
