993fcd1e71e14cd181aa3053ee8642f25ea4f580
Fazry <lhuqita.fazry@gmail.com>
2022-05-24 14:39:46 +0700

    fixing bugs

--- run_dancer_training_arxiv.sh
+++ run_dancer_training_arxiv.sh
@@ -8,7 +8,8 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --validation_file dataset/arxiv/processed/arxiv/val.json \
     --text_column document \
     --summary_column summary \
-    --output_dir dancer_arxiv_pre/models --logging_dir dancer_arxiv_pre/models/logs \
+    --output_dir dancer_arxiv_pre/models 
+    --logging_dir dancer_arxiv_pre/models/logs \
     --seed 100 \
     --per_device_train_batch_size=8 \
     --per_device_eval_batch_size=8 \
@@ -17,8 +18,15 @@ CUDA_VISIBLE_DEVICES=3 python src/run_summarization.py \
     --max_val_samples 251 \
     --learning_rate 1e-4 \
     --adafactor \
-    --max_source_length 512 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length \
+    --max_source_length 4096 
+    --max_target_length 128 
+    --val_max_target_length 128 
+    --pad_to_max_length \
     --num_beams 3 \
-    --num_train_epochs 2 --save_strategy epoch  --save_total_limit 1 \
+    --num_train_epochs 2 
+    --save_strategy epoch  
+    --save_total_limit 1 \
     --load_best_model_at_end \
-    --evaluation_strategy epoch --metric_for_best_model rouge2 --greater_is_better true
+    --evaluation_strategy epoch \
+    --metric_for_best_model rouge2 \
+    --greater_is_better true
