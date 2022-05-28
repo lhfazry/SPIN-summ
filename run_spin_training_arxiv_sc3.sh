4ba65df22904665a7efc601f0f860dc9f707b1fa
Fazry <lhuqita.fazry@gmail.com>
2022-05-28 20:20:57 +0700

    add sc3

---
+++ run_spin_training_arxiv_sc3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=1 python src/run_summarization.py \
+    --model_name_or_path google/bigbird-pegasus-large-pubmed \
+    --tokenizer_name google/bigbird-pegasus-large-pubmed \
+    --do_train \
+    --do_eval \
+    --task summarization \
+    --train_file dataset/arxiv/dancer/train.json \
+    --validation_file dataset/arxiv/dancer/val.json \
+    --text_column document \
+    --summary_column abstract \
+    --output_dir spin_arxiv/sc3/models \
+    --logging_dir spin_arxiv/sc3/models/logs \
+    --seed 100 \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
+    --gradient_accumulation_steps=16 \
+    --gradient_checkpointing true \
+    --fp16 true \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --learning_rate 1e-4 \
+    --optim adafactor \
+    --max_source_length 4096 \
+    --max_target_length 256 \
+    --val_max_target_length 256  \
+    --pad_to_max_length \
+    --num_beams 3 \
+    --num_train_epochs 1 \
+    --save_strategy epoch \
+    --save_total_limit 1 \
+    --load_best_model_at_end \
+    --evaluation_strategy epoch \
+    --metric_for_best_model rouge2 \
+    --greater_is_better true
