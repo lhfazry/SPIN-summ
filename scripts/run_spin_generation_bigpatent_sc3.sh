e7b63d0f0b348ba4c4433e0fd9f722d8c35a83dc
Fazry <lhuqita.fazry@gmail.com>
2022-06-06 17:45:54 +0700

    fixing bugs

--- scripts/run_spin_generation_bigpatent_sc3.sh
+++ scripts/run_spin_generation_bigpatent_sc3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=1 python src/dancer_generation2.py \
-    --mode dancer \
+CUDA_VISIBLE_DEVICES=1 python src/spin_generation.py \
+    --mode spin \
     --model_path spin_bigpatent/sc3/models \
     --output_path spin_bigpatent/sc3 \
     --data_path dataset/bigpatent/dancer/test.json \
@@ -9,6 +9,6 @@ CUDA_VISIBLE_DEVICES=1 python src/dancer_generation2.py \
     --write_rouge 0 \
     --seed 100 \
     --test_batch_size 4 \
-    --max_summary_length 128 \
+    --max_summary_length 256 \
     --max_source_length 4096 \
     --num_beams 5
